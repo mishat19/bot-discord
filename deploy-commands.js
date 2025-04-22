544882c3d7eca3661badb8476ae02d6f9d1a0a94
Mathis Brongniart <spidarstudiogames@gmail.com>
2025-04-22 22:13:10 +0200

    Déploiement commandes tests

---
+++ deploy-commands.js
@@ -0,0 +1,45 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+//Récupérer tous les dossiers de COMMANDS (utility)
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    //Récupérer tous les fichiers des dossiers de COMMANDS (ici ceux d'UTILITY)
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    // Prendre SlashCommandBuilder#toJSON() export de chaque commande pour deployment
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+//Interaction avec API Discord
+const rest = new REST().setToken(token);
+
+//Déployer commandes
+(async () => {
+    try {
+        console.log(`Rafraichissement ${commands.length} commandes application (/).`);
+
+        //La méthode PUT pour rafraichir toutes commandes du serveur avec le SET courant
+        const data = await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log(`Réussite rechargement ${data.length} commandes application (/).`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs'); //Lire dossier commandes
+const path = require('node:path'); //Aide accéder fichiers/dossiers
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 //const intents = new Discord.GatewayIntentBits(53608447);
 const client = new Client({ intents: [GatewayIntentBits.Guilds] }); //Guild référence à un serveur Discord
 
-//bot.on('ready', async () => { console.log(`${bot.user.tag} est connecté`);}) //Déclencher quand action
-client.once(Events.ClientReady, readyClient => {
-    console.log(`${readyClient.user.tag} est lancé !`);
-});
+client.commands = new Collection(); //Class Collection extension de map
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 client.login(token);
