37ef34282e71812b53a380ab1d88095c6a57baa2
Mathis Brongniart <spidarstudiogames@gmail.com>
2025-04-26 20:10:05 +0200

    Fonctionnalité MATCH terminée

--- commands/utility/matcher.js
+++ commands/utility/matcher.js
@@ -19,7 +19,9 @@ module.exports = {
         const target = interaction.options.getUser('utilisateur');
         const utilisateurDemande = interaction.user;
         const motif = interaction.options.getString('motif');
+        const channelName = `match-${cleanUsername(utilisateurDemande.username)}-${cleanUsername(target.username)}`;
 
+        //Envoi demande
         const envoyer = new ButtonBuilder()
             .setCustomId('envoyer')
             .setLabel('Envoyer')
@@ -32,6 +34,7 @@ module.exports = {
             .setEmoji('😨')
             .setStyle(ButtonStyle.Secondary)
 
+        //Envoi réponse
         const accepter = new ButtonBuilder()
             .setCustomId('accepter')
             .setLabel('J\'accepte')
@@ -44,11 +47,33 @@ module.exports = {
             .setEmoji('❌')
             .setStyle(ButtonStyle.Secondary)
 
+        //Envoi message channelMatch épinglé
+        const match = new ButtonBuilder()
+            .setCustomId('match')
+            .setLabel('Match')
+            //.setEmoji('💗')
+            .setStyle(ButtonStyle.Success)
+
+        const echec = new ButtonBuilder()
+            .setCustomId('echec')
+            .setLabel('Échec')
+            //.setEmoji('💥')
+            .setStyle(ButtonStyle.Secondary)
+
+        const signaler = new ButtonBuilder()
+            .setCustomId('signaler')
+            .setLabel('Signaler')
+            //.setEmoji('👁️‍🗨️')
+            .setStyle(ButtonStyle.Danger)
+
         const row = new ActionRowBuilder()
             .addComponents(envoyer, annuler)
 
         const reponseTarget = new ActionRowBuilder()
             .addComponents(accepter, refuser)
+
+        const matchEpingle = new ActionRowBuilder()
+            .addComponents(match, echec, signaler)
 
         const envoi = await interaction.reply({
             content: `<@${interaction.user.id}>\n Veux-tu vraiment envoyer une demande à ${target} ?\n Cette personne recevra ta demande en Message Privé afin de valider ou refuser ta demande, vérifie bien d'avoir bien expliqué le motif de ta demande !`,
@@ -67,14 +92,22 @@ module.exports = {
             userInteraction = true;
 
             if(confirmation.customId === 'envoyer') {
-                await interaction.followUp({ content: 'Ta demande a été envoyée ! Tu vas recevoir une confirmation en MP sous peu.', ephemeral: true});
+                //--------------------------------------------
+                //Vérification si channel déjà existant
+                const channelVerif = interaction.guild.channels.cache.find(ch => ch.name === channelName);
+                if(channelVerif){
+                    await interaction.followUp({ content: 'Tu as déjà une demande de match acceptée.', flags: MessageFlags.Ephemeral});
+                    await envoi.delete();
+                    return;
+                }
+                await interaction.followUp({ content: 'Ta demande a été envoyée ! Tu vas recevoir une confirmation en MP sous peu.', flags: MessageFlags.Ephemeral});
                 //await interaction.editReply({ content: 'Envoyé', components: []});
                 await envoi.delete();
                 try {
                     await interaction.user.send({ content: `${target} a bien reçu ta demande, j\'espère que tu recevras rapidement une réponse !` });
                 } catch (error) {
                     console.error(`Erreur lors de l'envoi du MP à l'envoyeur :`, error);
-                    await interaction.followUp({ content: 'Tu ne peux pas recevoir de Messages Privé.', ephemeral: true });
+                    await interaction.followUp({ content: 'Tu ne peux pas recevoir de Messages Privé.', flags: MessageFlags.Ephemeral});
                 }
 
                 try {
@@ -88,20 +121,25 @@ module.exports = {
                 }
             } else{
                 await interaction.editReply({ content: 'Annulation', components: []});
-                await interaction.followUp({ content: 'Tout va bien, cela n\'est pas toujours facile ! 😊', ephemeral: true });
+                await interaction.followUp({ content: 'Tout va bien, cela n\'est pas toujours facile ! 😊', flags: MessageFlags.Ephemeral});
             }
         } catch (error){
             if(!userInteraction){
                 console.error('Erreur lors de l\'attente de la confirmation :', error);
-                await interaction.editReply({ content: 'Tu as été trop long à te décider...', components: [], ephemeral: true });
+                await interaction.editReply({ content: 'Tu as été trop long à te décider...', components: [], flags: MessageFlags.Ephemeral});
             }
         }
 
         //RECEPTION REPONSE
 
-        const filter = i => i.user.id === target.id;
+        function cleanUsername(username) {
+            return username
+                .toLowerCase()               // tout en minuscules
+                .replace(/\s+/g, '-')         // remplace tous les espaces par des tirets
+                .replace(/[^a-z0-9\-]/g, ''); // enlève tout ce qui n'est pas lettre, chiffre ou tiret
+        }
 
-        // D'abord tu déclares ta fonction
+        const filter = i => i.user.id === target.id;
         const responseHandler = async (i) => {
             if (!filter(i)) return;
 
@@ -109,6 +147,18 @@ module.exports = {
                 await i.update({content: 'Vous avez accepté la demande.', components: []});
                 try {
                     await utilisateurDemande.send({content: `<@${target.id}> a accepté ta demande de match !`});
+                    const channelMatch = await interaction.guild.channels.create({
+                            parent: '1365735697851355296',
+                            name: `${channelName}`,
+                            type: 0
+                    });
+                    const messageMatch = await channelMatch.send({
+                        content: 'Bienvenue dans votre salon !',
+                        components: [matchEpingle],
+                        fetchReply: true //Récupérer message
+                    });
+                    await messageMatch.pin();
+
                 } catch (error) {
                     console.error('Erreur lors de l\'envoi du message privé à l\'autre utilisateur :', error);
                     await interaction.followUp({
@@ -129,11 +179,11 @@ module.exports = {
                 }
             }
 
-            // Et ici tu arrêtes bien le listener
+            //Arrêt du listener
             interaction.client.off('interactionCreate', responseHandler);
         };
 
-        // Puis tu ajoutes ton listener
+        //Ajout du listener
         interaction.client.on('interactionCreate', responseHandler);
     }
 }
