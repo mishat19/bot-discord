f29aa574b3b3d9cdda80a6fc18c7fcf2fe0de42d
Mathis Brongniart <spidarstudiogames@gmail.com>
2025-04-27 23:24:54 +0200

    Fonctionnalité points d'intérêts

---
+++ interets-test.js
@@ -0,0 +1,12 @@
+const { Events, MessageFlags } = require('discord.js');
+
+module.exports = async (client, reaction, user ) => {
+    // Vérifier si la réaction a été ajoutée par un utilisateur (et non par un bot)
+    if (!user.bot) {
+        // Exécuter l'action souhaitée
+        console.log(`${user.tag} a ajouté une réaction '👍' au message avec l'ID ${reaction.message.id} au message de ${reaction.message.author.username}.`);
+
+        const channel = reaction.message.channel;
+        await channel.send({ content: `<@${user.id}> a ajouté une réaction '👍' au message avec l'ID ${reaction.message.id} au message de <@${reaction.message.author.id}>.`, flags: MessageFlags.Ephemeral });
+    }
+};
