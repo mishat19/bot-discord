d4f7095a81f172d1eb4a4aaeb3d830df5030dce5
Mathis Brongniart <spidarstudiogames@gmail.com>
2025-04-29 00:12:37 +0200

    Refonte système ajout/suppresion pts intérêt

---
+++ reactions/interet-negatif.js
@@ -0,0 +1,24 @@
+const { MessageFlags } = require('discord.js');
+const { NegatifsInteretPoints } = require('../database/database');
+
+module.exports = async (client, reaction, user, removed ) => {
+    // Vérifier si la réaction a été ajoutée par un utilisateur (et non par un bot)
+    if (!user.bot) {
+        if(removed){
+            // Exécuter l'action souhaitée
+            await NegatifsInteretPoints(user.id, reaction.message.author.id, -1);
+            //console.log(`${user.tag} a supprimé une réaction '💔' au message avec l'ID ${reaction.message.id} au message de ${reaction.message.author.username}.`);
+
+            const channel = reaction.message.channel;
+            await channel.send({ content: `<@${user.id}> a retiré une réaction '💔' au message avec l'ID ${reaction.message.id} au message de <@${reaction.message.author.id}>.` });
+        } else{
+            // Exécuter l'action souhaitée
+            await NegatifsInteretPoints(user.id, reaction.message.author.id, 1);
+            //console.log(`${user.tag} a ajouté une réaction '💔' au message avec l'ID ${reaction.message.id} au message de ${reaction.message.author.username}.`);
+
+            const channel = reaction.message.channel;
+            await channel.send({ content: `<@${user.id}> a ajouté une réaction '💔' au message avec l'ID ${reaction.message.id} au message de <@${reaction.message.author.id}>.` });
+
+        }
+    }
+};
